c4cf2a23cd189d892ac67dece8cd260934644338
brown8391 <138756169+brown8391@users.noreply.github.com>
2023-07-19 13:54:54 +0900

    Update ExogenousQuantAnal.py

--- utility/QuantityData/ExogenousQuantAnal.py
+++ utility/QuantityData/ExogenousQuantAnal.py
@@ -15,11 +15,3 @@ for j in range(len(exoin)):
     exoinminusout[j] = exoin[j] - exoout[j]
     
 print(exoinminusout)
-
-# at least from studying this one case, it looks like the system always has
-# at least as much exogenous input as output. Thus, level 0 agents should be
-# more desparate to unload supply. Also, when analyzing agent quantity
-# performance rates, these excess supplies should be taken into account.
-###########
-# Second run was done and again, we have at least as much exo input as exo
-# output. In fact, this time there was always strictly more.
