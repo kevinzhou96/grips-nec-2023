0483e1c5f8a8a592c33dac2003fa64442ad4e08a
Kevin Zhou <khzhou96@gmail.com>
2023-07-20 14:19:52 +0900

    Create util.py
    Implements some utility functions for RLAgent-v0

---
+++ agents/under-development/RLAgent-v0/util.py
@@ -0,0 +1,21 @@
+import os
+import datetime
+
+def format_time(seconds : float | int) -> str:
+    periods = [
+        ('d',   60*60*24),
+        ('h',   60*60),
+        ('m',   60),
+        ('s',   1),
+    ]
+    strings = []
+    for period, period_len in periods:
+        if seconds >= period_len:
+            period_value, seconds = divmod(seconds, period_len)
+            strings.append(f"{int(period_value)}{period}")
+    if seconds > 0:
+        strings.append(f"{round(seconds*1000)} ms")
+    return ' '.join(strings)
+            
+def current_file_dir():
+    return os.path.dirname(os.path.realpath(__file__))
