c7e8b9db6eb692bc0ee1338a64aeff8bd3717cf4
Kevin Zhou <khzhou96@gmail.com>
2023-07-28 16:44:59 +0900

    Implement DictBetterObservationManager
    Adds a dictionary-based observation manager for better readability and mixing of different observation spaces (Discrete and Box)

--- agents/under-development/RLAgent-v0/observation.py
+++ agents/under-development/RLAgent-v0/observation.py
@@ -194,3 +194,130 @@ class BetterFixedPartnerNumbersObservationManager(ObservationManager):
         if env._n_consumers != self.n_consumers:
             return False
         return True
+
+
+@define(frozen=True)
+class DictBetterObservationManager(ObservationManager):
+    n_bins: int = 10
+    n_sigmas: int = 2
+    extra_checks: bool = True
+    n_prices: int = 2
+    negotiation_limit: int = 40
+    max_steps: int = 200
+    max_disposal_cost: float = 0.3
+    max_shortfall_penalty: float = 1.3
+    max_production_cost: int = 20
+    max_trading_price: int = 40
+    n_partners: int = field(init=False)
+    n_suppliers: int = field(init=False)
+    n_consumers: int = field(init=False)
+    max_quantity: int = field(init=False)
+    n_lines: int = field(init=False)
+    max_n_competitors: int = field(init=False)
+
+    def _get_max_n_competitors(self):
+        if isinstance(self.factory.n_agents_per_level, tuple):
+            max_agents_per_level = self.factory.n_agents_per_level[-1]
+        else:
+            max_agents_per_level = self.factory.n_agents_per_level
+        if isinstance(self.factory.n_competitors, tuple):
+            max_n_competitors = self.factory.n_competitors[-1]
+        else:
+            max_n_competitors = self.factory.n_competitors
+        return max(max_agents_per_level, max_n_competitors)
+
+    def __attrs_post_init__(self):
+        assert isinstance(self.factory, FixedPartnerNumbersOneShotFactory)
+        object.__setattr__(self, "n_suppliers", self.factory.n_suppliers)
+        object.__setattr__(self, "n_consumers", self.factory.n_consumers)
+        object.__setattr__(self, "max_quantity", self.factory.n_lines)
+        object.__setattr__(self, "n_lines", self.factory.n_lines)
+        object.__setattr__(self, "n_partners", self.n_suppliers + self.n_consumers)
+        object.__setattr__(self, "max_n_competitors", self._get_max_n_competitors())
+
+    def make_space(self) -> spaces.Space:
+        """
+        Creates the observation space.
+        
+        The observation space consists of:
+            - The quantity and price for each incoming offer
+            - The current needed quantity
+            - The current round of negotations
+            - The number of competitors
+            - The current relative simulation time
+            - The curent disposal cost (** how is this going to be represented? **)
+            - The current shortfall penalty (** how is this going to be represented? **)
+            - The relative price increase from input to output product
+            - Maybe: parameters of disposal cost and shortfall penalty distributions
+        """
+
+        return spaces.Dict(
+            {
+                "offers": spaces.MultiDiscrete(
+                    np.asarray([self.max_quantity + 1, self.n_prices] * self.n_partners).flatten()
+                ),
+                "needs": spaces.Discrete(self.max_quantity + 1),
+                "round": spaces.Discrete(self.negotiation_limit + 1),
+                "n_competitors": spaces.Discrete(self.max_n_competitors + 1),
+                "simulation_step": spaces.Discrete(self.max_steps + 1),
+                "disposal_cost": spaces.Box(low=0.0, high=self.max_disposal_cost),
+                "shortfall_penalty": spaces.Box(low=0.0, high=self.max_shortfall_penalty),
+                "production_cost": spaces.Discrete(self.max_production_cost + 1),
+                "input_trading_price": spaces.Box(low=0.0, high=self.max_trading_price),
+                "output_trading_price": spaces.Box(low=0.0, high=self.max_trading_price),
+            }
+        )
+    
+    def make_first_observation(self, awi: OneShotAWI):
+        return self.encode(awi.state)
+
+    def encode(self, state : OneShotState):
+        obs = dict()
+
+        partners = state.my_partners
+        partner_index = dict(zip(partners, range(len(partners))))
+        infos: list[NegotiationDetails] = [None] * len(partners)  # type: ignore
+        negotiation_round = 0
+        neg_relative_time = 0.0
+        partners = list(
+            itertools.chain(
+                state.current_negotiation_details["buy"].items(),
+                state.current_negotiation_details["sell"].items(),
+            )
+        )
+        for partner, info in partners:
+            infos[partner_index[partner]] = info
+            negotiation_round = max(negotiation_round, info.nmi.state.step)
+            neg_relative_time = max(neg_relative_time, info.nmi.state.relative_time)
+        offers = [
+            (0, 0)
+            if info is None or info.nmi.state.current_offer is None  # type: ignore
+            else (
+                int(info.nmi.state.current_offer[QUANTITY]),  # type: ignore
+                int(info.nmi.state.current_offer[UNIT_PRICE] - info.nmi.outcome_space.issues[UNIT_PRICE].min_value),  # type: ignore
+            )
+            for info in infos
+        ]
+        if self.extra_checks:
+            assert (
+                len(partners) == self.n_partners
+            ), f"{len(partners)=} while {self.n_partners=}: {partners=}"
+            assert (
+                len(infos) == self.n_partners
+            ), f"{len(infos)=} while {self.n_partners=}: {infos=}"
+            assert (
+                len(offers) == self.n_partners
+            ), f"{len(infos)=} while {self.n_partners=}: {offers=}"
+
+        obs["offers"] = np.asarray(offers).flatten()
+        obs["needs"] = max(0, state.needed_sales if state.level == 0 else state.needed_supplies)
+        obs["round"] = negotiation_round
+        obs["n_competitors"] = state.n_competitors
+        obs["simulation_step"] = state.current_step
+        obs["disposal_cost"] = np.asarray([min(state.disposal_cost, self.max_disposal_cost)])
+        obs["shortfall_penalty"] = np.asarray([min(state.shortfall_penalty, self.max_shortfall_penalty)])
+        obs["production_cost"] = min(state.profile.cost, self.max_production_cost)
+        obs["input_trading_price"] = np.asarray([min(state.trading_prices[state.my_input_product], self.max_trading_price)])
+        obs["output_trading_price"] = np.asarray([min(state.trading_prices[state.my_output_product], self.max_trading_price)])
+
+        return obs
